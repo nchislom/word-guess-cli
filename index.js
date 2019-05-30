163323358b4b12ebbd4127962ea97823f7c91ed6
NChislom <11775893+nchislom@users.noreply.github.com>
2019-05-30 17:19:22 -0400

    Corrected memory leak in 'letter already guessed' check

--- index.js
+++ index.js
@@ -33,63 +33,64 @@ var nextGuess = function(){
             }
         ]).then(function(userInput){
 
-            // The IF block should bypass further letter check logic if the letter
-            // was already guessed. !!! This leads to a memory leak -- commented out
-
             // Push user input into array of guessed letters
-            // if(!lettersGuessed.includes(userInput.letter)){
-            //     lettersGuessed.push(userInput.letter);
-            //     console.log(colors.yellow("Letter already guessed. Choose another..."));
-            //     nextGuess();
-            // }
+            if(!lettersGuessed.includes(userInput.letter)){
+                lettersGuessed.push(userInput.letter);
 
-            // Snapshot of word-state pre-guess
-            var wordBeforeGuess = word.showWord().replace(/,/g, "");
-            
-            // Apply guessed letter to word object
-            word.guessLetter(userInput.letter);
-            
-            // Snapshot or word-state post-guess
-            var wordAfterGuess = word.showWord().replace(/,/g, "");
-
-            // Compare before and after word-states
-            if(wordBeforeGuess === wordAfterGuess){
-                console.log(colors.red("\nINCORRECT"));
-                guessesLeft--;
-                console.log("Guesses Left: " + guessesLeft);
-            } else {
-                console.log(colors.green("\nCORRECT!"));
-                if(!word.showWord().includes("_")){
-                    gameOver = true;
+                // Snapshot of word-state pre-guess
+                var wordBeforeGuess = word.showWord().replace(/,/g, "");
+                
+                // Apply guessed letter to word object
+                word.guessLetter(userInput.letter);
+                
+                // Snapshot or word-state post-guess
+                var wordAfterGuess = word.showWord().replace(/,/g, "");
+    
+                // Compare before and after word-states
+                if(wordBeforeGuess === wordAfterGuess){
+                    console.log(colors.red("\nINCORRECT"));
+                    guessesLeft--;
+                    console.log("Guesses Left: " + guessesLeft);
+                } else {
+                    console.log(colors.green("\nCORRECT!"));
+                    if(!word.showWord().includes("_")){
+                        gameOver = true;
+                    }
                 }
-            }
-
-            // LOST scenario
-            if(guessesLeft < 1 && word.showWord().includes("_")){
-                console.log(colors.yellow("\nGame Over\n"));
-                console.log(colors.yellow("The word was: " + word.revealWord()));
-                console.log(colors.yellow("\nNext word..."));
-                guessesLeft = 10;
-                lettersGuessed = [];
-                var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-                word = new Word(randomWord);
-                nextGuess();
-
-            // WIN scenario
-            } else if(gameOver === true){
-                console.log(word.showWord());
-                console.log(colors.rainbow("\nYou got it right! Next word..."));
-                guessesLeft = 10;
-                lettersGuessed = [];
-                gameOver = false;
-                var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-                word = new Word(randomWord);
-                nextGuess();
-
-            // Continue recursive function game loop
+    
+                // LOST scenario
+                if(guessesLeft < 1 && word.showWord().includes("_")){
+                    console.log(colors.yellow("\nGame Over\n"));
+                    console.log(colors.yellow("The word was: " + word.revealWord()));
+                    console.log(colors.yellow("\nNext word..."));
+                    guessesLeft = 10;
+                    lettersGuessed = [];
+                    var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+                    word = new Word(randomWord);
+                    nextGuess();
+    
+                // WIN scenario
+                } else if(gameOver === true){
+                    console.log(word.showWord());
+                    console.log(colors.rainbow("\nYou got it right! Next word..."));
+                    guessesLeft = 10;
+                    lettersGuessed = [];
+                    gameOver = false;
+                    var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+                    word = new Word(randomWord);
+                    nextGuess();
+    
+                // Continue recursive function game loop
+                } else {
+                    nextGuess();
+                }
+                
             } else {
+                console.log(colors.yellow("Letter already guessed. Choose another..."));
+                console.log("Letters guessed thus far: " + lettersGuessed);
                 nextGuess();
             }
+
         });
     }
 }
